e20c5c9dcbc89f2a91dda51dce1a19bb96755bf2
Sirapob <blacking987@gmail.com>
2023-03-21 16:09:50 +0700

    Add prompt

--- MyProject3.java
+++ MyProject3.java
@@ -9,12 +9,21 @@ import java.util.Scanner;
  * @author sirapob
  */
 public class MyProject3 {
+ 
     
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name ");
+        String name = scanner.nextLine();
+        if (name.equals("Sirapob")) {
+            System.out.println("\"Welcome Sirapob!!\"");
+        }else{
+            System.out.println("Who are you????");
+        }
         System.out.println("This is new feautre");
         System.out.println("Bruh");
     }
