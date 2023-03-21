b70488d9fbbf2829e4f6ff3d26fbea83cda73828
Sirapob <blacking987@gmail.com>
2023-03-21 14:19:27 +0700

    Version1.3

--- MyProject3.java
+++ MyProject3.java
@@ -3,20 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package myproject3;
-
+import java.util.Scanner;
 /**
  *
  * @author sirapob
  */
 public class MyProject3 {
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String name =  scanner.nextLine();
         // TODO code application logic here
         System.out.println("Hello World");
         System.out.println("Hello Ing");
+        System.out.println("This is your name " + name);
     }
     
 }
