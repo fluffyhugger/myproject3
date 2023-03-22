074f00e9a43950f9820013d5f6ec50a88082e83b
Sirapob <blacking987@gmail.com>
2023-03-22 16:20:09 +0700

    test

--- MyProject3.java
+++ MyProject3.java
@@ -3,27 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package myproject3;
-
+import java.util.Scanner;
 /**
  *
  * @author sirapob
  */
 public class MyProject3 {
-<<<<<<< HEAD
-<<<<<<< HEAD
- 
-    
-=======
->>>>>>> parent of b70488d (Version1.3)
-=======
->>>>>>> parent of b70488d (Version1.3)
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name ");
         String name = scanner.nextLine();
@@ -37,16 +23,11 @@ public class MyProject3 {
         System.out.println("Feature 2 Comming soon");
         System.out.println("NAWWWW");
         System.out.println("I think i have to play some again");
-=======
-        // TODO code application logic here
+       // TODO code application logic here
+        System.out.println("Hello World");
+        System.out.println("Hello Ing");    // TODO code application logic here
         System.out.println("Hello World");
         System.out.println("Hello Ing");
->>>>>>> parent of b70488d (Version1.3)
-=======
-        // TODO code application logic here
-        System.out.println("Hello World");
-        System.out.println("Hello Ing");
->>>>>>> parent of b70488d (Version1.3)
     }
     
 }
